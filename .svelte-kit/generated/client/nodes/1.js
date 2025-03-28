39959e6d844bd6642b002703d2fb76b9bd45f0be
Jake <jake@Jakes-MacBook-Pro.local>
2025-03-28 14:02:47 -0500

    1

---
+++ .svelte-kit/generated/client/nodes/1.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.20.2_@sveltejs+vite-plugin-svelte@5.0.3_svelte@5.25.3_vite@6.2.3_@types_60225cc401b84e93a135b1166fcc18db/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
